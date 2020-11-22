a37efed814fd5bdc3dafb8445e362aa179e9f776
Christian Nuss <christian.nuss@gmail.com>
2020-11-21 18:03:16 -0800

    Allowed Origins

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
-const { SERVICE_NAME, STAGE } = process.env;
+const { SERVICE_NAME, STAGE, ALLOWED_ORIGINS } = process.env;
 
 const serviceName = SERVICE_NAME || 'unknown-service';
 const stage = STAGE || 'local';
+const allowedOrigins = ALLOWED_ORIGINS || null;
 
-export { serviceName as SERVICE_NAME, stage as STAGE };
+export { serviceName as SERVICE_NAME, stage as STAGE, allowedOrigins as ALLOWED_ORIGINS };
--- src/util.ts
+++ src/util.ts
@@ -1,11 +1,20 @@
+import { ALLOWED_ORIGINS } from './constants';
+
 export const createHeaders = (event: any) => {
   const headers: { [key: string]: string } = {};
 
-  if (event && event.httpMethod && event.headers.Host) {
+  if (event && event.httpMethod && event.headers.origin) {
     headers['Access-Control-Allow-Headers'] =
       'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent';
     headers['Access-Control-Allow-Methods'] = `OPTIONS,${event.httpMethod}`;
-    headers['Access-Control-Allow-Origin'] = event.headers.Host;
+    if (ALLOWED_ORIGINS) {
+      const origin = ALLOWED_ORIGINS.split(',').find(allowedOrigin => allowedOrigin === event.headers.origin);
+      if (origin) {
+        headers['Access-Control-Allow-Origin'] = origin;
+      }
+    } else {
+      headers['Access-Control-Allow-Origin'] = '*';
+    }
   }
   return headers;
 };

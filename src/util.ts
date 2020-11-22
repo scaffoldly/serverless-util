e451378d8da36e675fce29a39e75c541904addbd
Christian Nuss <christian.nuss@gmail.com>
2020-11-21 18:22:54 -0800

    Better logic for appending headers

--- src/util.ts
+++ src/util.ts
@@ -3,18 +3,23 @@ import { ALLOWED_ORIGINS } from './constants';
 export const createHeaders = (event: any) => {
   const headers: { [key: string]: string } = {};
 
-  if (event && event.httpMethod && event.headers.origin) {
-    headers['Access-Control-Allow-Headers'] =
-      'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent';
-    headers['Access-Control-Allow-Methods'] = `OPTIONS,${event.httpMethod}`;
-    if (ALLOWED_ORIGINS) {
-      const origin = ALLOWED_ORIGINS.split(',').find(allowedOrigin => allowedOrigin === event.headers.origin);
-      if (origin) {
-        headers['Access-Control-Allow-Origin'] = origin;
-      }
-    } else {
-      headers['Access-Control-Allow-Origin'] = '*';
+  headers['Access-Control-Allow-Headers'] =
+    'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent';
+
+  if (event && event.httpMethod) {
+    headers['Access-Control-Allow-Methods'] = event.httpMethod;
+  }
+
+  if (!ALLOWED_ORIGINS) {
+    headers['Access-Control-Allow-Origin'] = '*';
+  }
+
+  if (ALLOWED_ORIGINS && event && event.headers.origin) {
+    const origin = ALLOWED_ORIGINS.split(',').find(allowedOrigin => allowedOrigin === event.headers.origin);
+    if (origin) {
+      headers['Access-Control-Allow-Origin'] = origin;
     }
   }
+
   return headers;
 };

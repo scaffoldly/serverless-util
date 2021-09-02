a5931d12f4166115ab45337906c7056a02478415
Christian Nuss <christian.nuss@gmail.com>
2021-09-02 11:14:12 -0700

    localstack support

--- src/exports/aws.ts
+++ src/exports/aws.ts
@@ -2,6 +2,9 @@ import * as AWSXRay from 'aws-xray-sdk-core';
 import * as _AWS from 'aws-sdk';
 import { STAGE } from '../constants';
 import * as http from 'http';
+import { boolean } from 'boolean';
+
+const { LOCALSTACK = 'false' } = process.env;
 
 export const AWS = STAGE !== 'local' ? AWSXRay.captureAWS(_AWS) : _AWS;
 export const XRAY_ENV_TRACE_ID = '_X_AMZN_TRACE_ID';
@@ -9,3 +12,30 @@ export const XRAY_ENV_TRACE_ID = '_X_AMZN_TRACE_ID';
 if (STAGE !== 'local') {
   AWSXRay.captureHTTPsGlobal(http, true);
 }
+
+export const S3 = (region?: string) => {
+  if (!boolean(LOCALSTACK)) {
+    return new AWS.S3({ region: region });
+  }
+
+  console.warn('Using Localstack');
+  return new AWS.S3({ endpoint: 'http://localhost:4566', region });
+};
+
+export const SES = (region?: string) => {
+  if (!boolean(LOCALSTACK)) {
+    return new AWS.SES({ region: region });
+  }
+
+  console.warn('Using Localstack');
+  return new AWS.SES({ endpoint: 'http://localhost:4566', region });
+};
+
+export const SNS = (region?: string) => {
+  if (!boolean(LOCALSTACK)) {
+    return new AWS.SNS({ region: region });
+  }
+
+  console.warn('Using Localstack');
+  return new AWS.SNS({ endpoint: 'http://localhost:4566', region });
+};
